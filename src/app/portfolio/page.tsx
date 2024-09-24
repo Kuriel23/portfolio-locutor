fca9bdbf78b1ebf0081d2f3e15a50b6a2bf1349b
Kuriel <37979213+Kuriel23@users.noreply.github.com>
2024-09-25 00:31:45 +0100

    feat: filter servers by properties

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -41,66 +41,68 @@ export default function Servers() {
 					Clique nos servidores agora para ver o que eu fiz!
 				</h2>
 				<div className="grid grid-cols-1 lg:grid-cols-3 gap-x-2 gap-y-5 w-full grid-flow-dense">
-					{servers.map((server) => {
-						return (
-							<div
-								className={`bg-neutral-900 hover:bg-neutral-800 mr-3 pb-3 h-auto max-lg:w-full cursor-pointer rounded-md${server.isHighlight ? " lg:col-span-2" : ""}`}
-								key={server.name}
-								onClick={() => {
-									setSelectedServer(server);
-									onOpen();
-								}}
-							>
-								<img
-									alt="server banner"
-									src={server.bannerURL}
-									className="w-full h-full object-cover max-h-36 rounded-t-md pointer-events-none"
-								/>
-								<Image
-									alt="server logo"
-									isBlurred
-									src={server.avatarURL}
-									className="w-12 ml-3 -mt-8 rounded-xl pointer-events-none"
-								/>
-								<div className="w-full flex justify-between items-center px-3 mt-3">
-									<p className="text-white font-bold text-md gap-1.5 flex items-center">
-										{server.partnered && (
-											<Image
-												src={"/assets/partner.svg"}
-												width={16}
-												height={16}
-												alt="Partnered"
-												className="pointer-events-none"
-											/>
-										)}
-										{server.verified && (
-											<Image
-												src={"/assets/verified.svg"}
-												width={16}
-												height={16}
-												alt="Verified"
-												className="pointer-events-none"
-											/>
-										)}
-										{server.name}
-									</p>
-									<div className="flex bg-neutral-700 text-principal rounded-md p-1 gap-1 items-center opacity-25 hover:opacity-80 text-xs">
-										<MdLabel /> {server.tags[0]}
-									</div>
-								</div>
-								<p className="text-principal text-md mt-2 gap-1.5 px-3 flex items-center">
-									<Image
-										src={"/assets/members.svg"}
-										width={16}
-										height={16}
-										className="pointer-events-none"
-										alt="Members"
+					{servers
+						.filter((server) => server.isServersPage)
+						.map((server) => {
+							return (
+								<div
+									className={`bg-neutral-900 hover:bg-neutral-800 mr-3 pb-3 h-auto max-lg:w-full cursor-pointer rounded-md${server.isHighlight ? " lg:col-span-2" : ""}`}
+									key={server.name}
+									onClick={() => {
+										setSelectedServer(server);
+										onOpen();
+									}}
+								>
+									<img
+										alt="server banner"
+										src={server.bannerURL}
+										className="w-full h-full object-cover max-h-36 rounded-t-md pointer-events-none"
 									/>
-									{Math.ceil(server.members).toLocaleString("pt-BR")}{" "}
-								</p>
-							</div>
-						);
-					})}
+									<Image
+										alt="server logo"
+										isBlurred
+										src={server.avatarURL}
+										className="w-12 ml-3 -mt-8 rounded-xl pointer-events-none"
+									/>
+									<div className="w-full flex justify-between items-center px-3 mt-3">
+										<p className="text-white font-bold text-md gap-1.5 flex items-center">
+											{server.partnered && (
+												<Image
+													src={"/assets/partner.svg"}
+													width={16}
+													height={16}
+													alt="Partnered"
+													className="pointer-events-none"
+												/>
+											)}
+											{server.verified && (
+												<Image
+													src={"/assets/verified.svg"}
+													width={16}
+													height={16}
+													alt="Verified"
+													className="pointer-events-none"
+												/>
+											)}
+											{server.name}
+										</p>
+										<div className="flex bg-neutral-700 text-principal rounded-md p-1 gap-1 items-center opacity-25 hover:opacity-80 text-xs">
+											<MdLabel /> {server.tags[0]}
+										</div>
+									</div>
+									<p className="text-principal text-md mt-2 gap-1.5 px-3 flex items-center">
+										<Image
+											src={"/assets/members.svg"}
+											width={16}
+											height={16}
+											className="pointer-events-none"
+											alt="Members"
+										/>
+										{Math.ceil(server.members).toLocaleString("pt-BR")}{" "}
+									</p>
+								</div>
+							);
+						})}
 				</div>
 				<Modal
 					backdrop="blur"

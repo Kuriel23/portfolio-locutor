a3be4946023669f80ce77ef9f7826e7656b0e04a
Bruno Ramos <37979213+Kuriel23@users.noreply.github.com>
2025-02-21 19:51:27 +0000

    chore: use new heroui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,21 @@
-import { nextui } from '@nextui-org/react';
+import { heroui } from '@heroui/react';
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {
-      colors: {
-        principal: '#d1d1d1',
-      },
-    },
-  },
-  darkMode: 'class',
-  plugins: [nextui()],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+		"./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				principal: "#d1d1d1",
+			},
+		},
+	},
+	darkMode: "class",
+	plugins: [heroui()],
 };
 export default config;
